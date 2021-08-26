1fe8f4a9555cb1034adb220671a1883d445e43c4
Kim Hendrikse <kim@kiwiclan.com>
2021-08-26 19:54:15 +0200

    Correct spelling on reboot and shutdown C file names

--- sbts_install_stalkedbythestate.sh
+++ sbts_install_stalkedbythestate.sh
@@ -489,8 +489,8 @@ create_reboot_and_shutdown() {
     echo ""
 
     cd "$SUDO_USER_HOME/app/bin" || abort "Can't change to $SUDO_USER_HOME/app/bin"
-    gcc reboot.c -o reboot || abort "Can't compile reboot.c"
-    gcc shutdown.c -o shutdown || abort "Can't compile shutdown.c"
+    gcc sbts_reboot.c -o reboot || abort "Can't compile sbts_reboot.c"
+    gcc .sbts_shutdown.c -o shutdown || abort "Can't compile sbts_shutdown.c"
     mv reboot shutdown /usr/local/sbts-sbin || abort "Can't move reboot and shutdown to /usr/local/sbts-sbin"
     chown root:root "/usr/local/sbts-sbin/reboot" "/usr/local/sbts-sbin/reboot" || abort "Can't chown root:root /usr/local/sbts-sbin/reboot /usr/local/sbts-sbin/shutdown"
     chmod +s,g+s "/usr/local/sbts-sbin/reboot" "/usr/local/sbts-sbin/reboot" || abort "Can't chmod setuid root /usr/local/sbts-sbin/reboot and /usr/local/sbts-sbin/reboot"
