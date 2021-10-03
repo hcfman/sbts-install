69c0725c25191bb1abcbcdfc396420f9c85354d0
Kim Hendrikse <kim@kiwiclan.com>
2021-10-03 13:43:31 +0200

    Renamed multi-secure.py to sbts-secure.py

--- resources/secure/start_secure.sh
+++ resources/secure/start_secure.sh
@@ -12,7 +12,7 @@ HERE=$(dirname $0)
 cd "$HERE" || abort "Can't change to script directory"
 
 while [ 1 ] ; do
-    ./multi-secure.py -b 127.0.0.1 -p 8764 resources/config.json
+    ./sbts-secure.py -b 127.0.0.1 -p 8764 resources/config.json
     sleep 1
 done
 
