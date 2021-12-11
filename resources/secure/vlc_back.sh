b5b46dd823b5dd7564c9d80e77c5d6b077805a12
Kim Hendrikse <kim@kiwiclan.com>
2021-12-11 17:12:35 +0100

    Remove typos in scripts

--- resources/secure/vlc_back.sh
+++ resources/secure/vlc_back.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 while [ 1 ] ; do
-    cvlc  -v rtsp://myusername:mypassword.@192.168.1.65/PSI/streaming/channels/102 --http-reconnect \
+    cvlc  -v rtsp://myusername:mypassword@192.168.1.65/PSI/streaming/channels/102 --http-reconnect \
 	--sout-keep --sout '#duplicate{dst=std{access=http{mime=multipart/x-mixed-replace;boundary=--7b3cc56e5f51db803f790dad720ed50a,user=santa,pwd=youtube},mux=mpjpeg,dst=:8101/back/video.jpg}}' vlc://quit
     sleep 10
 done
