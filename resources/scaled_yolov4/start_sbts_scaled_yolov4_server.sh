bbde999ac60346d343a6d18cb1301cbf1393ac9c
Kim Hendrikse <kim@kiwiclan.com>
2022-07-11 20:00:16 +0200

    Change settings for letter boxing and image size

--- resources/scaled_yolov4/start_sbts_scaled_yolov4_server.sh
+++ resources/scaled_yolov4/start_sbts_scaled_yolov4_server.sh
@@ -14,6 +14,6 @@ cd $HERE || abort "Can't change to script directory"
 export OPENBLAS_CORETYPE=ARMV8
 
 while [ 1 ] ; do
-    ./sbts-scaled-yolov4-large-server.py -b 0.0.0.0 -p 8767 --device 0 --img-size 1536 --weights weights/yolov4-p7.pt
+    ./sbts-scaled-yolov4-large-server.py -b 0.0.0.0 -p 8767 -d --device 0 --img-size 1536 --weights weights/yolov4-p7.pt
     sleep 5
 done
